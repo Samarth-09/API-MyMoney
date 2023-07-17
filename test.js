30d83509258a38c3c6225cc29701658fce3acd87
Samarth-09 <srparekh0909@gmail.com>
2023-07-17 18:22:46 +0530

    changed-7

--- test.js
+++ test.js
@@ -17,15 +17,16 @@ mongoose.connect('mongodb+srv://samarth:samarth009@cluster0.twpsfoj.mongodb.net/
     const port = process.env.PORT || 3000;
     app.listen(port, ()=> {
         console.log(port);
-        app.post('/test/add', async (req, res)=> {
-            const t = new test_model({
-                name: req.body.name,
-                cpi:req.body.cpi
-            });
-            console.log(t);
-            await t.save();
-            const x = await test_model.find({name: req.body.name});
-            res.json(x[0].toString());
-        })
+        
+    })
+    app.post('/test/add', async (req, res)=> {
+        const t = new test_model({
+            name: req.body.name,
+            cpi:req.body.cpi
+        });
+        console.log(t);
+        await t.save();
+        const x = await test_model.find({name: req.body.name});
+        res.json(x[0].toString());
     })
 });
